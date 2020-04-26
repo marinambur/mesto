fe74aa5e6f5257ebbe13c714d6712a3859560c46
Marina Kuznetsova <marinaburykina@gmail.com>
2020-04-26 23:40:08 +0300

    bem

--- script/script.js
+++ script/script.js
@@ -4,24 +4,21 @@ const plus = document.querySelector('.profile__button-large');
 const closePic = document.querySelector('.picture-add__button-close');
 const button = document.querySelector('.profile__button-small');//находим кнопки
 const close = document.querySelector('.popup__button-close');
-const closeBigPicBtn = document.querySelector('.picture-big__button-close');
+const closeBigPicBtn = document.querySelector('.popup__button-close_big');
 const popup = document.querySelector('.popup');
 const formElement = popup.querySelector('.form');
 const formPictureAdd = document.querySelector('.form-add');
 const popupPictureAdd = document.getElementById("picture-add");//id попапов
 const popupInformation = document.getElementById("information");
 const popupPictureBig = document.getElementById("picture-big");
-const imageBig = document.querySelector('.picture-big__item');//нашли картинку в будущем поп-апе 
-const imageHeader = document.querySelector('.picture-big__name');//нашли название в будущем попапе, который сейчас откроем
-
+const imageBig = document.querySelector('.popup__item');//нашли картинку в будущем поп-апе 
+const imageHeader = document.querySelector('.popup__name');//нашли название в будущем попапе, который сейчас откроем
 const nameInput = document.querySelector('.text-form_name');//находим поля форм
 const jobInput = document.querySelector('.text-form_profession');
 const placeInput = document.querySelector('.place-form_name');//находим поля форм в форме добавления картинки
 const linkInput = document.querySelector('.place-form_link');
 const profileTitle = document.querySelector('.profile__title');//здесь и далее находим заголовки, куда будут перезаписываться данные, введенные в форму, а также части формы
 const profileSubtitle = document.querySelector('.profile__subtitle');
-
-
 const initialCards = [//архив 6 картинок, данных в ТЗ
 
     {
@@ -49,19 +46,23 @@ const initialCards = [//архив 6 картинок, данных в ТЗ
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-function PopupOpenClose(elem) {//открытие и закрытие всех форм
-    elem.classList.toggle('popup_opened');
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubtitle.textContent;
-    placeInput.value = placeInput.textContent;
-    linkInput.value = linkInput.textContent;
-}
+function togglePopup(elem) {//открытие и закрытие всех форм
+    if (elem.classList.contains('popup_opened')) {
+        elem.classList.toggle('popup_opened')
+    } else {
+        elem.classList.toggle('popup_opened');
+        nameInput.value = profileTitle.textContent;
+        jobInput.value = profileSubtitle.textContent;
+        placeInput.value = "";
+        linkInput.value = "";
+    }
+};
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    PopupOpenClose(popupInformation);
+    togglePopup(popupInformation);
 }//нажимаем на "сохранить", данные из формы сохраняются в заголовки, а потом закрывается попап
 
 function addCard(name, picture) {//собираем карточку
@@ -77,7 +78,7 @@ function addCard(name, picture) {//собираем карточку
 function formSubmitPictureAdd(evt) {//добавляем картинку и название из данных формы
     evt.preventDefault();
     gridContainer.prepend(addCard(placeInput.value, linkInput.value));//добавили картинку впереди остальных
-    PopupOpenClose(popupPictureAdd);//закрыли форму добавления картинки
+    togglePopup(popupPictureAdd);//закрыли форму добавления картинки
 }
 
 initialCards.forEach(function addCardFromArray(item) {//добавление картинок из цикла
@@ -94,17 +95,17 @@ function deleteCard(evt) {//функция удаления карточки
 
 function showPictureBig(evt) {//добавляем поп-ап увеличения той картинки, на которую мы нажали
     imageBig.src = evt.target.src;//адрес будущей картинки это адрес картинки в карточке 
-    imageHeader.textContent = evt.target.parentElement.nextElementSibling.firstElementChild.textContent;
-    PopupOpenClose(popupPictureBig);
+    imageHeader.textContent = evt.target.closest('.grid__box').lastElementChild.textContent;
+    togglePopup(popupPictureBig);
 }
 //кнопка плюса, листенеры на кнопку открытия и на крестик закрытия
-plus.addEventListener('click', () => PopupOpenClose(popupPictureAdd));
-closePic.addEventListener('click', () => PopupOpenClose(popupPictureAdd));
+plus.addEventListener('click', () => togglePopup(popupPictureAdd));
+closePic.addEventListener('click', () => togglePopup(popupPictureAdd));
 
 //кнопка карандаш, листенеры на кнопку открытия и на крестик закрытия
-button.addEventListener('click', () => PopupOpenClose(popupInformation));
-close.addEventListener('click', () => PopupOpenClose(popupInformation));
-closeBigPicBtn.addEventListener('click', () => PopupOpenClose(popupPictureBig));
+button.addEventListener('click', () => togglePopup(popupInformation));
+close.addEventListener('click', () => togglePopup(popupInformation));
+closeBigPicBtn.addEventListener('click', () => togglePopup(popupPictureBig));
 formElement.addEventListener('submit', formSubmitHandler);
 formPictureAdd.addEventListener('submit', formSubmitPictureAdd);
 
